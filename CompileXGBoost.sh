760be4a87650a93762218f4c19b4302ea9218034
Xiaoyong Zhu <xiaoyzhu@microsoft.com>
2017-08-14 17:17:06 -0700

    change compilation scripts

--- CompileXGBoost.sh
+++ CompileXGBoost.sh
@@ -1,20 +1,19 @@
-%%local
 #!/bin/bash 
+
+#Install the required packages 
 sudo apt-get update
 sudo apt-get install -y maven git build-essentials maven cmake python-setuptools
-sudo pip install xgboost
+#sudo pip install xgboost
+
+#clone XGBoost repo and build it
 git clone --recursive https://github.com/dmlc/xgboost
-
-cd xgboost
-
-
-#find * -name tracker.py -type f -print0 | xargs -0 sed -i '' "s/port=9091/port=33000/g"
-make -j4
-
-cd jvm-packages
-
-
+cd xgboost/jvm-packages
 mvn -DskipTests=true package
-mkdir -p /databricks/jars/
+
+
+#put the compiled packge to shared storage
+hadoop fs -put xgboost4j-spark/target/xgboost4j-spark-0.7-jar-with-dependencies.jar /
 cp xgboost4j-spark/target/xgboost4j-spark-0.7-jar-with-dependencies.jar /databricks/jars/xgboost4j-spark-0.7-with-dependencies.jar
-""", true)
+
+#put the sample data to shared storage
+hadoop fs -put xgboost/demo/data/agaricus.txt* /
